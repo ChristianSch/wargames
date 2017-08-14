5e28b03308c7d01761b575fe64194b8baad1f0a7
Christian Schulze <accounts@andinfinity.de>
2017-08-14 23:03:28 +0200

    added scripts to show/hide passwds

---
+++ hide_secrets.sh
@@ -0,0 +1,15 @@
+# encrypts password files.
+# `passwd.public` files are encrypted and written to `passwd.secret`,
+# the public file is then removed.
+EMAIL="SECRET"
+IN="passwds.public"
+OUT="passwds.secret"
+
+for basePath in $(find . -name $IN | sed "s/\.\/\(.*\)$IN/\1/"); do
+    echo $basePath$IN "to" $basePath$OUT
+    gpg --output $basePath$OUT --encrypt --user $EMAIL --yes $basePath$IN \
+        && test -e $basePath$OUT \
+        && rm $basePath"passwds.public" \
+        && echo " << pruned"
+done
+
---
+++ show_secrets.sh
@@ -0,0 +1,12 @@
+# decrypts password files.
+# level passwords are written as clear text into `passwds.public`
+# in their respective directory.
+EMAIL="SECRET"
+IN="passwds.secret"
+OUT="passwds.public"
+
+for basePath in $(find . -name "passwds.secret" | sed "s/\.\/\(.*\)$IN/\1/"); do
+    echo $basePath$IN "to" $basePath$OUT
+    gpg --output $basePath$OUT --yes --decrypt $basePath$IN 2&> /dev/null \
+        && echo " >> made visible"
+done
